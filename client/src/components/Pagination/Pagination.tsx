cfe524ecd62623536b878ebecb7239987a2d4282
Tatyana Pavlova <44174465+ttpavlova@users.noreply.github.com>
2025-12-06 01:17:49 +0300

    feat/client: move pagination logic to a hook

--- client/src/components/Pagination/Pagination.tsx
+++ client/src/components/Pagination/Pagination.tsx
@@ -1,3 +1,4 @@
+import { usePagination } from "./hooks/usePagination";
 import styles from "./Pagination.module.scss";
 import cls from "classnames";
 
@@ -16,36 +17,11 @@ export const Pagination = ({
 }: PaginationProps) => {
   const totalPages = Math.ceil(totalCount / limit);
 
-  const getMiddlePages = () => {
-    const middlePages = [];
-
-    // if current page has two previous pages and two next pages, include them in array
-    for (let i = -2; i <= 2; i++) {
-      if (page + i > 1 && page + i < totalPages) {
-        middlePages.push(page + i);
-      }
-    }
-
-    return middlePages;
-  };
-
-  const middlePages = getMiddlePages();
-
-  const getNewPage = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (event.target instanceof HTMLElement) {
-      const newPage = Number(event.target.textContent);
-
-      // validate number
-      if (
-        !isNaN(newPage) &&
-        newPage >= 1 &&
-        newPage <= totalPages &&
-        page !== newPage
-      ) {
-        handleChange(newPage);
-      }
-    }
-  };
+  const { middlePages, getNewPage } = usePagination(
+    page,
+    totalPages,
+    handleChange
+  );
 
   return (
     <div className={styles.pagination} onClick={(e) => getNewPage(e)}>
