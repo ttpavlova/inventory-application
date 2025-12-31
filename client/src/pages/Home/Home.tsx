379299d29ad0aa15edf8bf31161f579890f73124
Tatyana Pavlova <44174465+ttpavlova@users.noreply.github.com>
2025-12-31 03:36:36 +0300

    feat/client: add styles for the Home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,11 +1,27 @@
 import { Link } from "react-router-dom";
+import homeShoes from "@/assets/home-1.jpg";
+import homeCreateShoe from "@/assets/home-2.jpg";
 import styles from "./Home.module.scss";
 
 export const Home = () => {
   return (
     <div className={styles.home}>
-      <Link to="/shoes">Shoes</Link>
-      <Link to="/shoes/create">Add a Shoe</Link>
+      <Link className={styles.card} to="/shoes">
+        <div className={styles.coverWrapper}>
+          <img src={homeShoes} alt="Shoes page" className={styles.cover} />
+        </div>
+        <span className={styles.text}>Shoes</span>
+      </Link>
+      <Link className={styles.card} to="/shoes/create">
+        <div className={styles.coverWrapper}>
+          <img
+            src={homeCreateShoe}
+            alt="Create a shoe page"
+            className={styles.cover}
+          />
+        </div>
+        <span className={styles.text}>Add a Shoe</span>
+      </Link>
     </div>
   );
 };
