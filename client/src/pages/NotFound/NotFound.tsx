6f813d89b8648f9b0b89d1d46cd1794ceff9485a
Tatyana Pavlova <44174465+ttpavlova@users.noreply.github.com>
2026-01-08 01:18:42 +0300

    feat/client: update text and styles on Not Found page

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import { ArrowLeftOutlined } from "@ant-design/icons";
 import styles from "./NotFound.module.scss";
 
 export const NotFound = () => {
-  return <div className={styles.container}>This page doesn't exist</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>404 Error</h1>
+      <p className={styles.description}>
+        We couldn't find the page you're looking for
+      </p>
+
+      <Link to="/" className={styles.link}>
+        <div className={styles.linkContent}>
+          <ArrowLeftOutlined />
+          <span className={styles.linkText}>Back to Homepage</span>
+        </div>
+      </Link>
+    </div>
+  );
 };
